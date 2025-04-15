4e75064d94faec0077365f1a182c5d9fe860ce10
josephOri <ori100no@gmail.com>
2025-04-15 16:48:24 +0300

    added useEffect hook to pre fetch the articles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ArticleList from './components/ArticleList';
 import { SearchBar } from './components/SearchBar';
 import { useArticles } from './hooks/articleHooks';
+import { Box } from '@mui/material';
 
 const App = () => {
   const { data: articles = [], isLoading, error } = useArticles();
   const [displayedArticles, setdisplayedArticles] = useState(articles);
+
+  useEffect(() => {
+    setdisplayedArticles(articles);
+  }, [articles]);
 
   const handleSearch = (query: string) => {
     const filteredArticles = articles.filter(
@@ -21,10 +26,10 @@ const App = () => {
   if (error) return <div>Error loading articles: {error.message}</div>;
 
   return (
-    <div>
+    <Box>
       <SearchBar onSearch={handleSearch} />
       <ArticleList articles={displayedArticles} />
-    </div>
+    </Box>
   );
 };
 
